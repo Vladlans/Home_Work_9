25c0399839a310b41cc97759ead4f90954692de4
Vlad <blackmor143@yandex.ru>
2022-11-30 18:50:53 +0300

    Выполнили 1 задание

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+int InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine() ?? "0", out int number);
+        if (!result)
+        {
+            Console.WriteLine($"Ввод некорректный! Попробуйте ещё раз.");
+            Thread.Sleep(1500);
+            continue;
+        }
+        return number;
+    }
+}
+
+void PrintNumbers(int min, int max)
+{
+    if (max % 2 != 0) max = max - 1;
+    if (max == min)
+    {
+        Console.WriteLine("{0,4}", max);
+        return;
+    }
+    else
+    {
+        Console.WriteLine("{0,4}", min);
+    }
+    PrintNumbers(min + 2, max);
+}
+
+int n = 2;
+int m = InputNumber("Задайте число: ");
+PrintNumbers(n, m);
